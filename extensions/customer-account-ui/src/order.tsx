2f70e27863949dde75d14e6e056219c3c57512bb
Helge Johannsen <helgemmo@googlemail.com>
2023-12-13 19:44:06 +0100

    new customer-account-ui

--- extensions/customer-account-ui/src/order.tsx
+++ extensions/customer-account-ui/src/order.tsx
@@ -1,34 +1,35 @@
 import {
-    reactExtension,
-    Banner,
-    useOrder,
-    Button,
-    useApi,
-    useTotalAmount,
-  } from '@shopify/ui-extensions-react/customer-account';
-  
-  export default reactExtension(
-    'customer-account.order-status.block.render',
-    () => <Extension />,
-  );
-  
-  function Extension() {
-    const order = useOrder();
-    const cost = useTotalAmount();
-    const textAmount = `${cost.amount}`
-    const parameters = new URLSearchParams({
-        vendorid: "8403",
-        order_id: order.id.split("Order/")[1],
-        order_amount: textAmount,
-    })
-      //return `https://finanzieren.consorsfinanz.de/web/ecommerce/gewuenschte-rate?${parameters}`
-        const link = `https://finanzieren.consorsfinanz.de/web/ecommerce/gewuenschte-rate?${parameters}`
-    if (order) {
-      return (
-        <Button to={link}>Jetzt Finanzieren mit Consors Finanz</Button>
-      );
-    }
-  
-    return null;
+  reactExtension,
+  Banner,
+  useOrder,
+  Button,
+  useApi,
+  useTotalAmount,
+} from '@shopify/ui-extensions-react/customer-account';
+
+export default reactExtension(
+  'customer-account.order-status.block.render',
+  () => <Extension />,
+);
+
+function Extension() {
+  const order = useOrder();
+  const cost = useTotalAmount();
+  const textAmount = `${cost.amount}`
+  const order_id = order.id.split("Order/")[1]
+  const parameters = new URLSearchParams({
+      vendorid: "8403",
+      order_id: order_id,
+      order_amount: textAmount,
+      notifyURL: `https://cons-f-dev.cpro-server.de/api/public/notify/${order_id}`
+  })
+    //return `https://finanzieren.consorsfinanz.de/web/ecommerce/gewuenschte-rate?${parameters}`
+      const link = `https://finanzieren.consorsfinanz.de/web/ecommerce/gewuenschte-rate?${parameters}`
+  if (order) {
+    return (
+      <Button to={link}>Jetzt Finanzieren mit Consors Finanz</Button>
+    );
   }
-  
+
+  return null;
+}
