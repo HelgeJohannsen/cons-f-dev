aa7835f79ce12398d9163848a933b1365887f606
Helge <helgemmo@googlemail.com>
2023-12-20 15:49:53 +0100

    delete cancel queue

--- app/utils/shopify/webhooks/ordersCancel.ts
+++ app/utils/shopify/webhooks/ordersCancel.ts
@@ -26,21 +26,33 @@ export async function webbhook_oredersCancel(shop: string, payload: unknown) {
     const orderData = parseResult.data;
     console.log("parsed oderData", orderData);
     if (orderData.tags.includes("Consors Finanzierung")) {
-      console.log(
-        "Cancel order because it is Consors Finanzierung:",
-        orderData
-      );
-      const createdShopifyOrderCancelUnhandled =
-        await createShopifyOrderCancelUnhandled(
-          shop,
-          orderData.id,
-          orderData.admin_graphql_api_id,
-          orderData.current_total_price
+      console.log("Cancel order because it is Consors Finanzierung:",orderData );
+
+      const checkout = await getCheckoutByOrderId(orderData.id);
+      if (checkout == null) {
+        console.error("no checkout with given uuid in database", orderData.id);
+        return undefined;
+      }
+    
+      //const transactionId = await getTransactionId(shop, admin_graphql_api_id)
+      if (checkout.transaction_id != undefined) {
+        const transaction_id = checkout.transaction_id;
+        const response = await getConsorsClient(shop).then((consorsClient) =>
+          consorsClient?.stornoOrder(transaction_id)
         );
-      console.log(
-        "createdShopifyOrderCanceldUnhandled",
-        createdShopifyOrderCancelUnhandled
-      );
+        if (response === undefined) {
+          console.error(`No consors Client for shop ${shop}`);
+          return false;
+        } else if (response.status < 200 || response.status >= 300) {
+          console.error(
+            `non 2xx response(${response.status}) from consorsApi.stornoOrder : `,
+            await response.text()
+          );
+          return false;
+        } else {
+          return true;
+        }
+      }
     } else {
       console.log("keine Consors Finanzierung");
     }
@@ -49,62 +61,4 @@ export async function webbhook_oredersCancel(shop: string, payload: unknown) {
   }
 }
 
-interface OrderQueueEntry {
-  orderId: bigint;
-  shop: string;
-  admin_graphql_api_id: string;
-  current_total_price: string;
-  createdAt: Date;
-  counter: number;
-}
 
-export async function handleOrderCancelQueue({
-  shop,
-  orderId,
-  admin_graphql_api_id,
-  counter,
-  createdAt,
-}: OrderQueueEntry) {
-  console.log("handling orderCancelQueue Entry");
-
-  const runAt = new Date(createdAt.getTime());
-
-  runAt.setHours(runAt.getHours() + counter);
-
-  if (runAt.getTime() > Date.now()) {
-    console.log("skipping orderCancelQueue entry");
-    return false;
-  }
-
-  await incrementCounterShopifyOrderCancelUnhandled(orderId);
-
-  const oderId = BigInt(admin_graphql_api_id.split("Order/")[1]);
-  console.log("orderId from db", oderId);
-  const checkout = await getCheckoutByOrderId(oderId);
-  if (checkout == null) {
-    console.error("no checkout with given uuid in database", oderId);
-    return undefined;
-  }
-
-  //const transactionId = await getTransactionId(shop, admin_graphql_api_id)
-  if (checkout.transaction_id != undefined) {
-    const transaction_id = checkout.transaction_id;
-    const response = await getConsorsClient(shop).then((consorsClient) =>
-      consorsClient?.stornoOrder(transaction_id)
-    );
-    if (response === undefined) {
-      console.error(`No consors Client for shop ${shop}`);
-      return false;
-    } else if (response.status < 200 || response.status >= 300) {
-      console.error(
-        `non 2xx response(${response.status}) from consorsApi.stornoOrder : `,
-        await response.text()
-      );
-      return false;
-    } else {
-      return true;
-    }
-  } else {
-    console.error("no transaction id in metafield for ", admin_graphql_api_id);
-  }
-}
