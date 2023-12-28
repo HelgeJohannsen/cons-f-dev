9d4521fc87bdc8b7a671b4a570c02eba139fc68a
valdenicio22 <valdenicio22@gmail.com>
2023-12-28 22:58:33 +0100

    WIP - Testing fetching all collections

--- app/routes/api.public.productsByCollection.tsx
+++ app/routes/api.public.productsByCollection.tsx
@@ -3,9 +3,16 @@ import { authenticate } from "~/shopify.server";
 
 export async function loader({ request }: LoaderArgs) {
   const { admin, session } = await authenticate.admin(request);
-  const response = await admin.rest.resources.CustomCollection.all({
+  const shopifyResponse = await admin.rest.resources.CustomCollection.all({
     session: session,
   });
 
-  console.log("ALL COLLECTIONS: ", response);
+  console.log("ALL COLLECTIONS: ", shopifyResponse);
+
+  return new Response(JSON.stringify(shopifyResponse), {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+  });
 }
