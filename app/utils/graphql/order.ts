81215b21e492e57925f6a7f8b91331aa4bff36fd
Helge <helgemmo@googlemail.com>
2024-01-03 14:10:09 +0100

    create checkout by order

--- app/utils/graphql/order.ts
+++ app/utils/graphql/order.ts
@@ -4,18 +4,16 @@ export async function checkIfOrderExists(shop: string, order_id: string) {
   const orderGlobalIdentifier = "gid://shopify/Order/" + order_id
   const gqlClient = await getGraphqlClient(shop);
   const query = `{
-        order(id: "gid://shopify/Order/5534614749463"){
-            id
-            tags
-      }
-    }`;
-    console.log("orderGlobalIdentifier",orderGlobalIdentifier)
-  const name = await gqlClient.query({ data: { query } });
+    order(id: "gid://shopify/Order/5534614749463"){
+        tags
+  }
+}`;
+const metaFields = await gqlClient.query({ data: { query } });
+const tags = (metaFields.body as any)["data"]["order"]["tags"];
   console.log("name", name)
-  const orderID = (name.body as any)["data"]["order"]["tags"];
 
-  if (orderID == null) {
-    console.error("order does not exist", orderID);
+  if (tags == null) {
+    console.error("order does not exist", tags);
     return false;
   }
   return true;
