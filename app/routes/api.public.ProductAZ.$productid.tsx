972ec4ad6e86bd39a1344bafdcc5ff90cf56e99d
Helge <helgemmo@googlemail.com>
2024-01-03 12:41:50 +0100

    create checkout by order

--- app/routes/api.public.ProductAZ.$productid.tsx
+++ app/routes/api.public.ProductAZ.$productid.tsx
@@ -2,16 +2,21 @@ import type { LoaderArgs } from "@remix-run/node";
 import { authenticate } from "~/shopify.server";
 import { checkIfProductHasCollection } from "~/utils/graphql/product";
 
-export async function loader({ request }: LoaderArgs) {
+export async function loader({ request,params }: LoaderArgs) {
   const shop = "helge-test.myshopify.com"
-  const res = await checkIfProductHasCollection(shop,"")
+  if(params.productid != null){
+    const res = await checkIfProductHasCollection(shop, params.productid!)
 
-  console.log("res: ", res);
+    console.log("res: ", res);
+  
+    return new Response(JSON.stringify(res), {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    });
+  }else{
+    console.log("productid is null" , params.productid)
+  }
 
-  return new Response(JSON.stringify(res), {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json",
-    },
-  });
 }
